e394c37790d3d778768a0fbd18c3a0adecf13d4a
jordicros <jordicros997@gmail.com>
2019-03-14 21:45:42 +0100

    CORS al localhost:4200

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,17 @@ const bodyParser = require('body-parser')
 const app = express()
 const api = require('./routes')
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin","http://localhost:4200");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+    }
+    next()
+})
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use('/api',api)
