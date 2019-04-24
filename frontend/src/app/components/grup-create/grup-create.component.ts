9ff2bc405ccd4ac6345447ba5697bc589abbb978
mbasart <mbd2797@gmail.com>
2019-04-24 16:50:23 +0200

    ja es veu bonica la createGrupForm, falta que funcioni

--- frontend/src/app/components/grup-create/grup-create.component.ts
+++ frontend/src/app/components/grup-create/grup-create.component.ts
@@ -4,7 +4,9 @@ import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 @Component({
   selector: 'app-grup-create',
   templateUrl: './grup-create.component.html',
-  styleUrls: ['./grup-create.component.scss'],
+  styleUrls: ['./grup-create.component.scss','../../res/fonts/util.css','../../res/vendor/bootstrap/css/bootstrap.min.css','../../res/fonts/font-awesome-4.7.0/css/font-awesome.min.css',
+  '../../res/fonts/iconic/css/material-design-iconic-font.min.css','../../res/vendor/animate/animate.css','../../res/vendor/css-hamburgers/hamburgers.min.css', '../../res/vendor/animsition/css/animsition.min.css',
+  '../../res/vendor/select2/select2.min.css','../../res/vendor/daterangepicker/daterangepicker.css']
 })
 export class GrupCreateComponent implements OnInit {
 
