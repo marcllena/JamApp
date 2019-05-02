d5cde87539cfcc1027feaed32491a035a5464ac4
victorpinon <victorpinonrattia@hotmail.com>
2019-05-02 11:45:21 +0200

    arreglat error en un port

--- frontend/src/app/services/environment.ts
+++ frontend/src/app/services/environment.ts
@@ -2,6 +2,6 @@ import configParam from '../../../configParam';
 
 export class Environment {
 
-  urlUser: string = "http://"+configParam.constants().BACKEND_IP+":3001/api/";
+  urlUser: string = "http://"+configParam.constants().BACKEND_IP+":3000/api/";
   //urlUser: string = "http://localhost:3001/api/";
 }
