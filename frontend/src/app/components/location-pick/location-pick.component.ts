02a1a315cc493d441821a27c9feaefb3f6c92a08
Marc <marcllena@hotmail.com>
2019-05-02 13:58:37 +0200

    Agafir Location Component

---
+++ frontend/src/app/components/location-pick/location-pick.component.ts
@@ -0,0 +1,119 @@
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Geolocation} from "@ionic-native/geolocation/ngx";
+import {NativeGeocoder} from "@ionic-native/native-geocoder/ngx";
+import {ToolbarService} from "../../services/toolbar.service";
+import {Platform} from "@ionic/angular";
+import {UserServices} from "../../services/user.services";
+
+declare var google;
+
+@Component({
+  selector: 'app-location-pick',
+  templateUrl: './location-pick.component.html',
+  styleUrls: ['./location-pick.component.scss'],
+})
+export class LocationPickComponent implements OnInit {
+
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  marker:any
+  latitud: number;
+  longitud: number;
+  clickedLatitud: number;
+  clickedLongitud: number;
+
+
+  constructor(
+    private geolocation: Geolocation,
+    private nativeGeocoder: NativeGeocoder,
+    private toolbarService: ToolbarService,
+    public platform: Platform,
+    private userService: UserServices,
+  ) {
+    this.clickedLatitud= -360;
+    this.clickedLongitud= -360;
+  }
+
+  ngOnInit() {
+    this.obtindreUbicacio();
+  }
+
+  refresh(){
+    this.obtindreUbicacio();
+  }
+
+  obtindreUbicacio(){
+    //Si es un mobil
+    if(this.platform.is('cordova')){
+      this.geolocation.getCurrentPosition().then((resp) => {
+        this.latitud = resp.coords.latitude;
+        this.longitud = resp.coords.longitude;
+        this.loadMap();
+        return;
+      }).catch((error) => {
+        console.log('Error getting location', error);
+      });
+    }
+    else if(this.platform.is('desktop')){
+      //Si es desktop
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((resp) => {
+          this.latitud = resp.coords.latitude;
+          this.longitud = resp.coords.longitude;
+          this.loadMap();
+          return
+        });
+      }
+      else {
+        console.log("Geolocation is not supported by this browser.");
+      }
+    }
+    //Si no tirem de  geolocalització per IP
+    this.userService.getUserLocation()
+      .subscribe(response => {
+        console.log("Resposta de la API "+ response)
+        if(response.status == 200){
+          this.latitud = response.body['lat'];
+          this.longitud = response.body['lon']
+          this.loadMap();
+          return
+        }
+      },err=>{
+        if(err.status == 500){
+          console.log("Error");
+          //Estem fotuts, inventem ubicació
+          this.latitud=41.2800161;
+          this.longitud=1.9766294;
+
+        }
+      })
+
+  }
+
+  loadMap() {
+    this.map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: this.latitud , lng: this.longitud},
+      zoom: 12,
+      styles: [
+        {
+          "featureType": "poi",
+          "stylers": [
+            { "visibility": "off" }
+          ]
+        }
+      ]
+    });
+    google.maps.event.addListener(this.map,"click", (event) => {
+      this.clickedLongitud=event.latLng.lng();
+      this.clickedLatitud=event.latLng.lat();
+      this.placeMarker();
+    });
+  }
+  placeMarker(){
+    var marker = new google.maps.Marker({
+      position: {lat: this.clickedLatitud , lng: this.clickedLongitud},
+      map: this.map,
+    });
+  }
+
+}
