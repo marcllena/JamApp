8440384c0bdf4ae8b315c7cf27190b38ac162613
Marc <marcllena@hotmail.com>
2019-05-13 12:50:52 +0200

    Afagir Component Filters, i avis abans de fer LogOut

---
+++ frontend/src/app/components/filter-form/filter-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-form',
+  templateUrl: './filter-form.component.html',
+  styleUrls: ['./filter-form.component.scss','../../res/fonts/util.css','../../res/vendor/bootstrap/css/bootstrap.min.css','../../res/fonts/font-awesome-4.7.0/css/font-awesome.min.css',
+    '../../res/fonts/iconic/css/material-design-iconic-font.min.css','../../res/vendor/animate/animate.css','../../res/vendor/css-hamburgers/hamburgers.min.css', '../../res/vendor/animsition/css/animsition.min.css',
+    '../../res/vendor/select2/select2.min.css','../../res/vendor/daterangepicker/daterangepicker.css']
+})
+export class FilterFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- frontend/src/app/services/toolbar.service.ts
+++ frontend/src/app/services/toolbar.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Router} from "@angular/router";
 import {DataService} from "./data.services";
+import { AlertController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class ToolbarService {
   private userId: string;
   private userName: string;
 
-  constructor(private router: Router,private singleton: DataService) {
+  constructor(private router: Router,private singleton: DataService, private alertController: AlertController) {
     this.singleton.newUserId.subscribe(userid => this.userId = userid);
     this.singleton.newUsername.subscribe(username => this.userName = username);
   }
@@ -19,10 +20,32 @@ export class ToolbarService {
     this.router.navigateByUrl('api/settings');
   }
 
-  logOut(){
-    localStorage.removeItem('token');
-    localStorage.removeItem('_id');
-    this.router.navigateByUrl('');
+  async logOut() {
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      message: '¿Seguro que desea salir?.',
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: () => {
+            console.log('Cancelamos Log Out');
+          }
+        }, {
+          text: 'OK',
+          handler: () => {
+            console.log('Realizamos Log Out');
+            localStorage.removeItem('token');
+            localStorage.removeItem('_id');
+            this.router.navigateByUrl('');
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  settings(){
+    this.router.navigateByUrl('api/filter');
   }
 
   getUserName(): string {
