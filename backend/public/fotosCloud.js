f42120aca6043bd4983abf3af64598a7759bcbe6
mbasart <mbd2797@gmail.com>
2019-06-07 11:47:04 +0200

    estic amb les fotos, pero encara falta

---
+++ backend/public/fotosCloud.js
@@ -0,0 +1,6 @@
+var imgPreview = document.getElementById('img-preview');
+var fileUpload = document.getElementById('file-upload');
+
+fileUpload.addEventListener('change',function(event){
+    console.log(event);
+})
