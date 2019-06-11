16e0428fe42ea67d9e9c461aaebdd87e3c40c215
mbasart <mbd2797@gmail.com>
2019-06-11 23:26:46 +0200

    afegides unes quantes coses a jam-create

--- frontend/src/app/components/jam-create/jam-create.component.ts
+++ frontend/src/app/components/jam-create/jam-create.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-jam-create',
   templateUrl: './jam-create.component.html',
-  styleUrls: ['./jam-create.component.scss'],
+  styleUrls: ['./jam-create.component.scss','../../res/fonts/util.css','../../res/vendor/bootstrap/css/bootstrap.min.css','../../res/fonts/font-awesome-4.7.0/css/font-awesome.min.css',
+  '../../res/fonts/iconic/css/material-design-iconic-font.min.css','../../res/vendor/animate/animate.css','../../res/vendor/css-hamburgers/hamburgers.min.css', '../../res/vendor/animsition/css/animsition.min.css',
+  '../../res/vendor/select2/select2.min.css','../../res/vendor/daterangepicker/daterangepicker.css'],
 })
 export class JamCreateComponent implements OnInit {
 
-  constructor() { }
+  createJamForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.createJamForm = this.formBuilder.group({
+
+    })
+   }
 
   ngOnInit() {}
 
