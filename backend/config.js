d399d85f0206fb74695fdfa2a9b4246eaacfc331
gabriel gonzalez <gabrigoca@gmail.com>
2020-03-12 10:23:13 +0100

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -5,5 +5,5 @@ module.exports={
     port: process.env.PORT || 3000,
     //db: process.env.MONGODB ||  'mongodb://database:27017/jamapp',
     db: process.env.MONGODB ||  'mongodb://localhost:27017/jamapp',
-    SECRET_TOKEN: '¡¡¡Sup3rCl4v3J4mAPP!!!'
+    SECRET_TOKEN: 'Your_Secret_Key'
 }
